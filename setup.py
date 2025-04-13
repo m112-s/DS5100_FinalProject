943372a8d7ef8d0c8d809ca4f5b2d06afe55fe74
Manav_Sheth <manavsheth112@gmail.com>
2025-04-13 19:41:19 -0400

    setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Manav Sheth", 
     author_email="gfw8hq@virginia.edu",
     description="Final Project", 
-    long_description=open("readme.md").read(), 
+    long_description=open("README.md").read(), 
     long_description_content_type="text/markdown",
     url="https://github.com/m112-s/DS5100-Final-Project", 
     packages=find_packages(),
